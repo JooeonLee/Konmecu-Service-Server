b1c83838470782479ba319c06bb64fee5e8d6adc
JooeonLee <cacophony0609@gmail.com>
2024-04-29 10:15:25 +0900

    Add : 프로젝트 초기화 및 관련 파일 추가

---
+++ src/main/java/jpabasic/konmecuserviceserver/KonmecuServiceServerApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.konmecuserviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonmecuServiceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonmecuServiceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpabasic/konmecuserviceserver/KonmecuServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.konmecuserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonmecuServiceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
